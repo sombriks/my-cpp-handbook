37878d50fb58bd2eb1639dddad9da45123f40c5b
Leonardo Silveira <sombriks@gmail.com>
2025-02-04 11:39:38 -0300

    basic io

---
+++ exercises/0007-basic-file-io/01-file-read.cc
@@ -0,0 +1,15 @@
+// 01-file-read.cc
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv)
+{
+  std::ifstream someNumber("some-numbers.bin");
+  long number = 0;
+  someNumber >> number;
+  std::cout << "Number is " << number << std::endl;
+  someNumber >> number;
+  std::cout << "Second Number is " << number << std::endl;
+  someNumber.close();
+  return 0;
+}
---
+++ exercises/0007-basic-file-io/02-file-read.cc
@@ -0,0 +1,10 @@
+// 02-file-read.cc
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv)
+{
+  std::ifstream someWords("some-words.txt");
+
+  return 0;
+}
