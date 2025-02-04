5c5ae0f51df462f259318e6fc6987cbe56c6cde0
Leonardo Silveira <sombriks@gmail.com>
2025-02-04 13:47:16 -0300

    file io

--- exercises/0007-basic-file-io/02-file-read.cc
+++ exercises/0007-basic-file-io/02-file-read.cc
@@ -1,10 +1,23 @@
 // 02-file-read.cc
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main(int argc, char **argv)
 {
   std::ifstream someWords("some-words.txt");
-
+  std::string str;
+  someWords >> str;
+  std::cout << str << std::endl;
+  someWords >> str;
+  std::cout << str << std::endl;
+  someWords >> str;
+  std::cout << str << std::endl;
+  // someWords >> str;
+  // std::cout << str << std::endl;
+  someWords.get(); // skip the endline
+  char line[20];
+  someWords.getline(line, 20);
+  std::cout << line << std::endl;
   return 0;
 }
