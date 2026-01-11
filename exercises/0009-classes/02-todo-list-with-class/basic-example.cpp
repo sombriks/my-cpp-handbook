4bcee22fa9d535c3a505cc13b9ec4956c8220a0a
Leonardo Silveira <sombriks@gmail.com>
2026-01-11 20:13:58 -0300

    docs on classes

--- exercises/0009-classes/02-todo-list-with-class/basic-example.cpp
+++ exercises/0009-classes/02-todo-list-with-class/basic-example.cpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <iostream>
 
 class TodoItem
 {
@@ -6,4 +6,76 @@ class TodoItem
   bool completed;
   TodoItem *next;
   TodoItem *prev;
+
+public:
+  TodoItem *toNext();
+  TodoItem *toPrev();
+  void complete();
+  static void display(TodoItem *head);
+  static void addItem(TodoItem *&head, const std::string &description);
 };
+
+void TodoItem::complete()
+{
+  completed = true;
+}
+
+TodoItem *TodoItem::toNext()
+{
+  return next;
+}
+
+TodoItem *TodoItem::toPrev()
+{
+  return prev;
+}
+
+void TodoItem::display(TodoItem *head)
+{
+  TodoItem *temp = head;
+  int index = 0;
+  while (temp)
+  {
+    std::cout << index++                                //
+              << ". [" << (temp->completed ? 'x' : ' ') //
+              << "] " << temp->description << '\n';
+    temp = temp->next;
+  }
+}
+
+void TodoItem::addItem(TodoItem *&head, const std::string &description)
+{
+  TodoItem *newItem = new TodoItem();
+  newItem->description = description;
+  newItem->completed = false;
+  newItem->next = nullptr;
+  newItem->prev = nullptr;
+  if (!head)
+  {
+    head = newItem;
+  }
+  else
+  {
+    TodoItem *temp = head;
+    while (temp->next)
+    {
+      temp = temp->next;
+    }
+    temp->next = newItem;
+    newItem->prev = temp;
+  }
+}
+
+int main()
+{
+  TodoItem *head = nullptr;
+  TodoItem::addItem(head, "Learn C++");
+  TodoItem::addItem(head, "Build a project");
+  TodoItem::addItem(head, "Contribute to open source");
+
+  head->toNext()->complete(); // Mark the second item as completed
+
+  TodoItem::display(head);
+
+  return 0;
+}
